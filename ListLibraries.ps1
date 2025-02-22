cc99fa7af7225c1d5831e4b590f3501eaa8aeeeb
Christopher <wallandall@users.noreply.github.com>
2025-02-22 20:33:19 +0100

    Delete ListLibraries.ps1

--- ListLibraries.ps1
+++
@@ -1,6 +0,0 @@
-$siteUrl = "https://yourtenant.sharepoint.com/sites/yoursite"
-
-Connect-PnPOnline -Url $siteUrl -Interactive
-
-# Get all document libraries
-Get-PnPList | Where-Object {$_.BaseType -eq "DocumentLibrary"} | Select Title
